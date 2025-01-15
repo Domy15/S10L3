ac4ccfe8503f0c07d5233512e3f6cba92efa7b84
Domy15 <domenico2016dellaventura@gmail.com>
2025-01-15 17:06:47 +0100

    commit

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { Alert } from "bootstrap/dist/js/bootstrap.min";
+
 const TOKEN =
   "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzVjMDAxY2QyMjA3MTAwMTVkZTJmNTUiLCJpYXQiOjE3MzY5NTE1OTgsImV4cCI6MTczODE2MTE5OH0.CvmnzKh3LRWMNEcLb0mRW1vLGkHSOHCamVUJf6rUv5o";
 
@@ -11,6 +12,7 @@ function MovieDetails() {
 
   const [film, setFilm] = useState({});
   const [filmDetails, setFilmDetails] = useState([]);
+  const [loading, setLoading] = useState(true); // Aggiungi uno stato per gestire il caricamento
 
   const getFilm = async () => {
     try {
@@ -36,8 +38,6 @@ function MovieDetails() {
       if (response.ok) {
         const data = await response.json();
         setFilmDetails(data);
-        console.log(filmDetails);
-        
       } else {
         throw new Error("Errore nei dati raccolti");
       }
@@ -47,9 +47,21 @@ function MovieDetails() {
   };
 
   useEffect(() => {
+    setLoading(true); // Imposta il caricamento su true quando inizia il fetch
     getFilm();
     getFilmDetails();
   }, [params.movieID]);
+
+  // Dopo che i dati sono stati caricati, imposta loading su false
+  useEffect(() => {
+    if (film && filmDetails) {
+      setLoading(false);
+    }
+  }, [film, filmDetails]);
+
+  if (loading) {
+    return <div className="spinner-border text-secondary" role="status"></div>; // Mostra una schermata di caricamento fino al termine
+  }
 
   return (
     <div className="row justify-content-center">
@@ -60,17 +72,22 @@ function MovieDetails() {
           <p className="card-text">{film.Plot}</p>
         </div>
       </div>
-      <div className="col-6">
-        {filmDetails.length > 0 ? (
+
+      {filmDetails ? (
+        <div className="col-6">
           <ul>
             {filmDetails.map((film) => (
-              <li className="text-white" key={film._id}>{film.comment}</li>
+              <li className="text-white" key={film._id}>
+                {film.comment}
+              </li>
             ))}
           </ul>
-        ) : (
+        </div>
+      ) : (
+        <div className="col-6">
           <Alert variant="info">There are no comments</Alert>
-        )}
-      </div>
+        </div>
+      )}
     </div>
   );
 }
